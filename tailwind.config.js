1123240db77006d3d302a0612f445528419fb6e7
505laura <106522200+505laura@users.noreply.github.com>
2023-02-06 11:38:12 +0000

    Add custom tailwind themes

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ['./src/**/*.{js,html,jsx}'],
+  theme: {
+    extend: {
+      colors: {
+        charlestonGreen: '#12252B',
+        offWhite: '#C0BCB5',
+        violet: '#71677C',
+      }
+    },
+  },
+  plugins: [],
+}
